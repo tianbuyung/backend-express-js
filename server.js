ca282a45d3d96ca7c0fef719c06dc7451988ef3e
Septian Maulana <septianm028@gmail.com>
2023-07-23 20:04:51 +0700

    refactor: handling 404 error with app.all middleware

--- server.js
+++ server.js
@@ -101,8 +101,15 @@ const three = (req, res) => {
 app.get("/chain(.html)?", [one, two, three]);
 
 // All routes not defined will be redirected to the 404
-app.get("/*", (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
 
 // error handlers
