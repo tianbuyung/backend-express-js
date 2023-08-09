7b18668c2149f63b2d4e03394f6df4f3a4ecb16d
Septian Maulana <septianm028@gmail.com>
2023-08-09 09:20:01 +0700

    feat: define root router

--- server.js
+++ server.js
@@ -52,58 +52,10 @@ app.use(express.static(path.join(__dirname, "/public")));
 // link css or any static public files into subdir
 app.use("/subdir", express.static(path.join(__dirname, "/public")));
 
+// import root router
+app.use("/", require("./routes/root"));
 // import subdir router
 app.use("/subdir", require("./routes/subdir"));
-
-// Define routes (express handling routes like waterfall)
-// app.get("/", (req, res) => {
-//   // res.sendFile("./views/index.html", { root: __dirname });
-//   res.sendFile(path.join(__dirname, "views", "index.html"));
-// });
-
-// This route is for the homepage and regex can understand the path with "/" or "/index" or "/index.html"
-app.get("^/$|/index(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
-});
-
-// This route is for the new-page and regex can understand the path with "/new-page" or "/new-page.html"
-app.get("/new-page(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "new-page.html"));
-});
-
-app.get("/old-page(.html)?", (req, res) => {
-  res.redirect(301, "/new-page.html"); // by default redirect to new-page.html with status code 302
-});
-
-// routes handler
-app.get(
-  "/hello(.html)?",
-  (req, res, next) => {
-    console.log("attempted to load hello.html");
-    next();
-  },
-  (req, res) => {
-    res.send("Hello World!");
-  }
-);
-
-// chaining route handlers
-const one = (req, res, next) => {
-  console.log("one");
-  next();
-};
-
-const two = (req, res, next) => {
-  console.log("two");
-  next();
-};
-
-const three = (req, res) => {
-  console.log("three");
-  res.send("Finished!");
-};
-
-app.get("/chain(.html)?", [one, two, three]);
 
 // All routes not defined will be redirected to the 404
 app.all("*", (req, res) => {
